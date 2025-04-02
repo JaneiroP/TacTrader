1f0d8da9897602df541245646bd8ca36f7731d63
janeiro.placido <placidoreynosoj@gmail.com>
2025-04-02 11:01:30 -0400

    Reapply "Rename 'Users' app to 'user_manager' and add ResUsers model."
    This reverts commit dd38ff0a381db6793feeed03bc56a4766ec175f8.

--- user_manager/apps.py
+++ user_manager/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsersConfig(AppConfig):
+class UserManagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Users'
+    name = 'user_manager'
